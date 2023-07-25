6faa9cfb77ae2f92dac80c8e52f399ec9b06ab8d
DHRUVIK LATHIYA <dhruviklathiya9999@gmail.com>
2023-07-25 10:27:42 +0530

    Update

--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
-// Core modules that is why can be imported anywhere when needed
-const http = require("http");
+// ===~===~===~===~===~===~===~===~===~===~===~===~===~===~===
+// ==>        Creating server with node core module        <==
+// ===~===~===~===~===~===~===~===~===~===~===~===~===~===~===
+
+// http is Core modules that is why can be imported anywhere when needed
+// const http = require("http");
 
 // http.createServer(function(req,res){
 //     res.write("Hello this is server created from http module");
 //     res.end("End");
 // }).listen(8080);
 
-// 3rd party module [npm installed]
+// ===~===~===~===~===~===~===~===~===~===~===~===~===~===~===
+// ==> Creating server with Node's framework : Express js  <==
+// ===~===~===~===~===~===~===~===~===~===~===~===~===~===~===
+
 const express = require("express");
 
 // Initializing express  
@@ -15,16 +22,13 @@ const app = express();
 app.listen(8080);
 // Following block of code will be run when landing on http://localhost:8080/
 app.get("", (req, res) => {
-    res.send("Server is running with express js");
+    res.send("Server is running...!!!"+"<br>"+"Try adding /greet to url"+"<br>"+"Try adding /bye to url");
 });
 // Following block of code will be run when landing on http://localhost:8080/greet
 app.get("/greet", (req, res) => {
-    res.send("Hello");
+    res.send("Hello...!!!"+"<br>"+"Replace greet with bye in url");
 });
 // Following block of code will be run when landing on http://localhost:8080/bye
 app.get("/bye", (req, res) => {
-    res.send("Bye!!! Catch you later");
+    res.send("Bye...!!!"+"<br>"+"Replace bye with greet in url");
 });
-
-
-
